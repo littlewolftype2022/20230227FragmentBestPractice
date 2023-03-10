eec262d47d5eda33a9e30c59a2579a5b820a774e
littlewolftype2022 <2997886697@qq.com>
2023-03-10 20:56:06 +0800

    Initial commit for 20230227FragmentBestPractice

---
+++ app/src/main/java/com/example/a20230227fragmentbestpractice/News.java
@@ -0,0 +1,28 @@
+package com.example.a20230227fragmentbestpractice;
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String setTitle(String title){
+        this.title = title;
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+
+
+}
